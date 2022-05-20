1375b5af5526cc1ef4c833ab92eb87e2bfbbf703
Song HwanSung <fansung@gmail.com>
2022-05-20 09:30:59 +0900

    컬럼 사이즈 사용자 조절 가능하도록

--- DataSpider.MANAGER/UserMonitor/CurrentTagValueMonitorDGV.cs
+++ DataSpider.MANAGER/UserMonitor/CurrentTagValueMonitorDGV.cs
@@ -66,7 +66,7 @@ namespace DataSpider.UserMonitor
             // None 로 해야 사용자 컬럼 사이즈 조절이 가능함. 
             // 바인딩 후 AutoResizeColumns(DataGridViewAutoSizeColumnsMode.DisplayedCells); 처리
             //            dataGridView_Main.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
-            dataGridView_Main.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            dataGridView_Main.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
             dataGridView_Main.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
             dataGridView_Main.AllowUserToResizeRows = dataGridView_Main.AllowUserToResizeColumns = true;
 
