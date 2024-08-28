a61863da1b10c460330e2a75c1b9ae522f85167a
정다영 <jdy8704@conwell.co.kr>
2024-08-28 17:55:30 +0900

    MA_COMMON_CD 인터페이스 기능 개발 중

---
+++ DataSpider.MANAGER/UserMonitor/CommonCodeConfig.Designer.cs
@@ -0,0 +1,346 @@
+﻿
+namespace DataSpider.UserMonitor
+{
+    partial class CommonCodeConfig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.tableLayoutConfigtagGroupMain = new System.Windows.Forms.TableLayoutPanel();
+            this.label_Title = new System.Windows.Forms.Label();
+            this.tableLayoutContents = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button_Remove = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button_Edit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button_Add = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.button_Close = new System.Windows.Forms.Button();
+            this.dataGridCommonCode = new System.Windows.Forms.DataGridView();
+            this.CodeGroup = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Code = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CodeName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CodeValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UpdateRegDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UpdateRegId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RegDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RegId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableLayoutConfigtagGroupMain.SuspendLayout();
+            this.tableLayoutContents.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCommonCode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutConfigtagGroupMain
+            // 
+            this.tableLayoutConfigtagGroupMain.ColumnCount = 1;
+            this.tableLayoutConfigtagGroupMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutConfigtagGroupMain.Controls.Add(this.label_Title, 0, 0);
+            this.tableLayoutConfigtagGroupMain.Controls.Add(this.tableLayoutContents, 0, 1);
+            this.tableLayoutConfigtagGroupMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutConfigtagGroupMain.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutConfigtagGroupMain.Name = "tableLayoutConfigtagGroupMain";
+            this.tableLayoutConfigtagGroupMain.RowCount = 2;
+            this.tableLayoutConfigtagGroupMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.253359F));
+            this.tableLayoutConfigtagGroupMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.74664F));
+            this.tableLayoutConfigtagGroupMain.Size = new System.Drawing.Size(870, 582);
+            this.tableLayoutConfigtagGroupMain.TabIndex = 0;
+            // 
+            // label_Title
+            // 
+            this.label_Title.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label_Title.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_Title.Font = new System.Drawing.Font("맑은 고딕", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label_Title.Location = new System.Drawing.Point(0, 0);
+            this.label_Title.Margin = new System.Windows.Forms.Padding(0);
+            this.label_Title.Name = "label_Title";
+            this.label_Title.Size = new System.Drawing.Size(870, 48);
+            this.label_Title.TabIndex = 1;
+            this.label_Title.Text = "Common Code Config";
+            this.label_Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutContents
+            // 
+            this.tableLayoutContents.ColumnCount = 2;
+            this.tableLayoutContents.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutContents.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutContents.Controls.Add(this.tableLayoutPanel1, 1, 0);
+            this.tableLayoutContents.Controls.Add(this.dataGridCommonCode, 0, 0);
+            this.tableLayoutContents.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutContents.Location = new System.Drawing.Point(0, 48);
+            this.tableLayoutContents.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutContents.Name = "tableLayoutContents";
+            this.tableLayoutContents.RowCount = 1;
+            this.tableLayoutContents.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutContents.Size = new System.Drawing.Size(870, 534);
+            this.tableLayoutContents.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel6, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(673, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(194, 528);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button_Remove);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 123);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(188, 54);
+            this.panel3.TabIndex = 2;
+            // 
+            // button_Remove
+            // 
+            this.button_Remove.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button_Remove.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_Remove.Location = new System.Drawing.Point(0, 0);
+            this.button_Remove.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_Remove.Name = "button_Remove";
+            this.button_Remove.Size = new System.Drawing.Size(188, 54);
+            this.button_Remove.TabIndex = 5;
+            this.button_Remove.Text = "Remove";
+            this.button_Remove.UseVisualStyleBackColor = true;
+            this.button_Remove.Click += new System.EventHandler(this.button_Remove_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button_Edit);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 63);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(188, 54);
+            this.panel2.TabIndex = 1;
+            // 
+            // button_Edit
+            // 
+            this.button_Edit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button_Edit.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_Edit.Location = new System.Drawing.Point(0, 0);
+            this.button_Edit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_Edit.Name = "button_Edit";
+            this.button_Edit.Size = new System.Drawing.Size(188, 54);
+            this.button_Edit.TabIndex = 5;
+            this.button_Edit.Text = "Edit";
+            this.button_Edit.UseVisualStyleBackColor = true;
+            this.button_Edit.Click += new System.EventHandler(this.button_Edit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button_Add);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(188, 54);
+            this.panel1.TabIndex = 0;
+            // 
+            // button_Add
+            // 
+            this.button_Add.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button_Add.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_Add.Location = new System.Drawing.Point(0, 0);
+            this.button_Add.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_Add.Name = "button_Add";
+            this.button_Add.Size = new System.Drawing.Size(188, 54);
+            this.button_Add.TabIndex = 4;
+            this.button_Add.Text = "Add";
+            this.button_Add.UseVisualStyleBackColor = true;
+            this.button_Add.Click += new System.EventHandler(this.button_Add_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.button_Close);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(3, 471);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(188, 54);
+            this.panel6.TabIndex = 5;
+            // 
+            // button_Close
+            // 
+            this.button_Close.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button_Close.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_Close.Location = new System.Drawing.Point(0, 0);
+            this.button_Close.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_Close.Name = "button_Close";
+            this.button_Close.Size = new System.Drawing.Size(188, 54);
+            this.button_Close.TabIndex = 7;
+            this.button_Close.Text = "Close";
+            this.button_Close.UseVisualStyleBackColor = true;
+            this.button_Close.Click += new System.EventHandler(this.button_Close_Click);
+            // 
+            // dataGridCommonCode
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridCommonCode.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridCommonCode.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridCommonCode.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CodeGroup,
+            this.Code,
+            this.CodeName,
+            this.CodeValue,
+            this.UpdateRegDate,
+            this.UpdateRegId,
+            this.RegDate,
+            this.RegId});
+            this.dataGridCommonCode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridCommonCode.Location = new System.Drawing.Point(3, 3);
+            this.dataGridCommonCode.Name = "dataGridCommonCode";
+            this.dataGridCommonCode.RowTemplate.Height = 23;
+            this.dataGridCommonCode.Size = new System.Drawing.Size(664, 528);
+            this.dataGridCommonCode.TabIndex = 3;
+            // 
+            // CodeGroup
+            // 
+            this.CodeGroup.HeaderText = "Code Group";
+            this.CodeGroup.Name = "CodeGroup";
+            this.CodeGroup.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.CodeGroup.Width = 180;
+            // 
+            // Code
+            // 
+            this.Code.HeaderText = "Code";
+            this.Code.Name = "Code";
+            this.Code.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Code.Width = 180;
+            // 
+            // CodeName
+            // 
+            this.CodeName.HeaderText = "Code Name";
+            this.CodeName.Name = "CodeName";
+            this.CodeName.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.CodeName.Width = 200;
+            // 
+            // CodeValue
+            // 
+            this.CodeValue.HeaderText = "Code Value";
+            this.CodeValue.Name = "CodeValue";
+            this.CodeValue.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.CodeValue.Width = 350;
+            // 
+            // UpdateRegDate
+            // 
+            this.UpdateRegDate.HeaderText = "Update Reg Date";
+            this.UpdateRegDate.Name = "UpdateRegDate";
+            this.UpdateRegDate.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.UpdateRegDate.Width = 80;
+            // 
+            // UpdateRegId
+            // 
+            this.UpdateRegId.HeaderText = "Update Reg Id";
+            this.UpdateRegId.Name = "UpdateRegId";
+            this.UpdateRegId.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.UpdateRegId.Width = 80;
+            // 
+            // RegDate
+            // 
+            this.RegDate.HeaderText = "Reg Date";
+            this.RegDate.Name = "RegDate";
+            this.RegDate.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.RegDate.Width = 80;
+            // 
+            // RegId
+            // 
+            this.RegId.HeaderText = "Reg Id";
+            this.RegId.Name = "RegId";
+            this.RegId.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.RegId.Width = 80;
+            // 
+            // CommonCodeConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 582);
+            this.Controls.Add(this.tableLayoutConfigtagGroupMain);
+            this.Name = "CommonCodeConfig";
+            this.Text = "ConfigTagGroup";
+            this.Load += new System.EventHandler(this.CommonCodeConfig_Load);
+            this.tableLayoutConfigtagGroupMain.ResumeLayout(false);
+            this.tableLayoutContents.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCommonCode)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutConfigtagGroupMain;
+        private System.Windows.Forms.Label label_Title;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutContents;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button_Remove;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button_Edit;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_Add;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button button_Close;
+        private System.Windows.Forms.DataGridView dataGridCommonCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CodeGroup;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Code;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CodeName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CodeValue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UpdateRegDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UpdateRegId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RegDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RegId;
+    }
+}
---
+++ DataSpider.MANAGER/UserMonitor/CommonCodeConfig.cs
@@ -0,0 +1,59 @@
+﻿using DataSpider.PC00.PT;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSpider.UserMonitor
+{
+    public partial class CommonCodeConfig : Form
+    {
+        private PC00Z01 sqlBiz = new PC00Z01();
+
+        public CommonCodeConfig()
+        {
+            InitializeComponent();
+        }
+
+        private void CommonCodeConfig_Load(object sender, EventArgs e)
+        {
+            InitControls();
+        }
+
+        private void InitControls()
+        {
+            string strErrCode = string.Empty;
+            string strErrText = string.Empty;
+
+            DataTable dtCommonCd = sqlBiz.GetAllCommonCode(ref strErrCode, ref strErrText);
+
+        }
+
+        private void button_Add_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Edit_Click(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        private void button_Remove_Click(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        private void button_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
